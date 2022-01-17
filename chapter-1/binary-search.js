0c5d074a79cf6ff356c8e5b1405463e7b5627e35
Tara Matthew <taramatthew.93@gmail.com>
2022-01-17 13:01:54 +0000

    Create binary search algorithm

---
+++ chapter-1/binary-search.js
@@ -0,0 +1,33 @@
+function binarySearch(list, item) {
+    // Low and high keep track of which part of the list to search in
+    let low = 0;
+    let high = list.length - 1;
+    let mid;
+    let guess;
+
+    // While not narrowed down to 1 element
+    while (low <= high) {
+        // Check the middle element
+        mid = Math.floor((low + high) / 2);
+        guess = list[mid];
+
+        // Found the item
+        if (guess == item) {
+            return mid;
+        }
+
+        // Guess was too high
+        if (guess > item) {
+            high = mid - 1;
+        } else {
+            // Guess was too low
+            low = mid + 1;
+        }
+    }
+
+    return null;
+}
+
+const list = [2, 5, 6, 7];
+
+console.log(binarySearch(list, 7));
