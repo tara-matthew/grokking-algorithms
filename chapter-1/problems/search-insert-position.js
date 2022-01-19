58a919bba6d27fa10667a216dc75e4d7173b2755
Tara Matthew <taramatthew.93@gmail.com>
2022-01-19 19:55:52 +0000

    Add SearchInsertPosition solution

---
+++ chapter-1/problems/search-insert-position.js
@@ -0,0 +1,35 @@
+/** https://leetcode.com/problems/search-insert-position/ **/
+
+/** Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * You must write an algorithm with O(log n) runtime complexity. **/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let searchInsert = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            low = mid + 1;
+        }
+
+        if (nums[mid] > target) {
+            high = mid - 1;
+        }
+    }
+
+    return low;
+};
+
+console.log(searchInsert([3, 5, 6, 8, 9], 7));
+console.log(searchInsert([4, 8, 11], 11));
