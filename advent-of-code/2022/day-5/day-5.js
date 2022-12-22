02d9ae21d40583d8795f8d265c9f19c4e7dc33f8
Tara Matthew <tara@tutorful.co.uk>
2022-12-22 17:29:17 +0000

    Update day 5 of AoC

--- advent-of-code/2022/day-5/day-5.js
+++ advent-of-code/2022/day-5/day-5.js
@@ -1,77 +1,36 @@
-const completeArray = [['N','Z'],['D','C','M'], ['P']];
-// console.log(completeArray[0]);
-
 const fs = require("fs");
 let matrix = fs.readFileSync("advent-of-code/2022/day-5/day-5.txt")
     .toString()
-    // .replace(/\n$/, '')
+    .replace(/\n$/, '')
     .replace(/[\[\]']+/g," ")
-    // .replace(/  +/g, ' ')
     .split("\n")
 
-const instructionsLines = matrix.slice(5); // hardcoded for now
-// console.log(instructionsLines);
+const TRANSPOSED_ARRAY_LENGTH = 3; // needs to be dynamic
+const INPUT_WIDTH = 3; // needs to be dynamic
+const INSTRUCTIONS_POSITION = 5;
+
+const instructionsLines = matrix.slice(INSTRUCTIONS_POSITION); // needs to be dynamic
 const instructions = new Map;
 
 for (let i = 0; i < instructionsLines.length; i ++) {
-    console.log(instructionsLines[i])
     instructions.set(i, instructionsLines[i].match(/\d+/g))
 }
 
-console.log(instructions)
+const array = [];
+for (let i = 0; i < INPUT_WIDTH; i ++) {
+    array[i] = [];
+    for (let j = 0; j < INPUT_WIDTH; j ++) {
+        let calculation = j * 4 + 1 // needs to be dynamic
+        array[i][j] = matrix[i].charAt(calculation);
+    }
+}
 
-// calculate the width by getting the length of the longest row (and then take away some space value) - or just use the numbers
-// const width = matrix[2].length - 2;
-// console.log(matrix[0]);
-// check the chars, work out based on how many spaces there are before the letter where it is in the row
-// console.log(matrix[0]);
-// or get the char position of the final leter in the row letter, then can work out the spaces?
-// can declare the length of the row?
-// console.log(matrix[0].charAt(2));
-
-const arr = new Array(3);
-const arr2 = new Array(3);
-const arr3 = new Array(3);
-// arr.fill(new Array(4))
-arr[0] = matrix[0].charAt(1)
-arr[1] = matrix[0].charAt(5)
-arr[2] = matrix[0].charAt(9)
-
-arr2[0] = matrix[1].charAt(1)
-arr2[1] = matrix[1].charAt(5)
-arr2[2] = matrix[1].charAt(9)
-
-arr3[0] = matrix[2].charAt(1)
-arr3[1] = matrix[2].charAt(5)
-arr3[2] = matrix[2].charAt(9)
-// arr[3] = matrix[0].charAt(6)
-// const totalArray = [...arr, ...arr2];
-const totalArray = []
-totalArray[0] = arr;
-totalArray[1] = arr2;
-totalArray[2] = arr3;
-// console.log(totalArray)
-
-// then make a new array based off this information?
-// let newArray = [];
-// newArray[0] = ' D '
-// console.log(newArray);
-
-// Get just the first row to iterate columns first
-// var t = matrix[0].map(function (col, c) {
-//     // For each column, iterate all rows
-//     return matrix.map(function (row, r) {
-//         return matrix[r][c];
-//     });
-// });
-
-function transposeArray(array, arrayLength) {
+const transposeArray = (array, arrayLength) => {
     let newArray = [];
-    // console.log(array.length)
-    for(let i = 0; i < array.length; i++) {
+
+    for (let i = 0; i < array.length; i++) {
         newArray.push([]);
     }
-    console.log(newArray);
 
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < arrayLength; j++){
@@ -82,9 +41,25 @@ function transposeArray(array, arrayLength) {
     return newArray;
 }
 
-// const maxLen = matrix.reduce((max, {length}) => Math.max(max, length), 0);
-// console.log(transposeArray(matrix, 3));
+const transposedArrays = [];
+for (let i = 0; i < INPUT_WIDTH; i ++) {
+    transposedArrays.push(transposeArray(array, TRANSPOSED_ARRAY_LENGTH)[i])
+}
 
-console.log(transposeArray(totalArray, 3)[0])
+const newTransposedArray = transposedArrays.map(array => array.filter(element => element !== '' && element !== ' '))
 
-// console.log(matrix[0]);
+// console.log(transposedArray, newTransposedArray);
+
+// const moveOneFromNumberTwoToNumberOne = newTransposedArray[0].unshift(newTransposedArray[1].shift())
+// console.log(newTransposedArray);
+
+instructions.forEach((line, index) => {
+    for (let i = 0; i < line[0]; i ++) {
+        newTransposedArray[line[2] - 1].unshift(newTransposedArray[line[1] - 1].shift())
+    }
+})
+
+console.log(newTransposedArray);
+
+// console.log(instructions.get(0))
+
