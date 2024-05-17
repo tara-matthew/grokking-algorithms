18f54918546cfa59db60fa88713e1196167dad83
tara-matthew <taramatthew.93@gmail.com>
2024-05-17 15:28:30 +0100

    Add merge sort algorithm and diagram

--- fundamentals/sorting/merge-sort/merge-sort.ts
+++ fundamentals/sorting/merge-sort/merge-sort.ts
@@ -1,6 +1,25 @@
-const mergeSort = (array) => {
+// const mergeSort = (array) => {
+//
+// }
 
+const merge = (leftArray: Array<number>, rightArray: Array<number>) => {
+    const sortedArray = [];
+
+    while (leftArray.length && rightArray.length) {
+        if (leftArray[0] <= rightArray[0]) {
+            sortedArray.push(leftArray.shift());
+        } else {
+            sortedArray.push(rightArray.shift());
+        }
+    }
+
+    return [...sortedArray, ...leftArray, ...rightArray]
 }
 
-const array = [2,5,4,1,3];
-console.log(mergeSort(array));
+// const array = [2,5,4,1,3];
+// console.log(mergeSort(array));
+
+const leftArray = [2,5, 7, 9];
+const rightArray = [4, 6];
+
+console.log(merge(leftArray, rightArray));
